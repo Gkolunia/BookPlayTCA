af6baf6261ad8bb6fe0d56dce93c8a910770010f
Gkolunia <Gkolunia@gmail.com>
2024-07-31 23:10:33 +0300

    initial commit

---
+++ BookPlayTCA/BookPlayTCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookPlayTCAApp.swift
+//  BookPlayTCA
+//
+//  Created by Mykola Hrybeniuk on 31.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct BookPlayTCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
