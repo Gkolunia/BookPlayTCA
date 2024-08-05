34bb2ea20a4b7f882d4fadb42c130133710e2f70
Gkolunia <Gkolunia@gmail.com>
2024-08-05 22:45:31 +0300

    Add tests

---
+++ BookPlayTCATests/BookPlayMainTests.swift
@@ -0,0 +1,82 @@
+//
+//  BookPlayMainTests.swift
+//  BookPlayTCATests
+//
+//  Created by Mykola Hrybeniuk on 05.08.2024.
+//
+
+import ComposableArchitecture
+import XCTest
+@testable import BookPlayTCA
+
+final class BookPlayMainTests: XCTestCase {
+    
+    @MainActor
+    func testBasics() async {
+        let state = BookPlayMainReducer.State.init(metadataUrlString: "MetadataUrl", playerState: .init())
+        
+        let store = TestStore(initialState: state) {
+            BookPlayMainReducer()
+        }
+        
+        await store.send(.changeScreenType) {
+            $0.isLyricsScreenMode = true
+        }
+        
+        await store.send(.screenLoaded) {
+            $0.downloadMode = .initialDownloading
+        }
+        
+        await store.receive(\.downloadMetaData) {
+            $0.downloadMode = .downloaded
+            $0.keyPoints = Metadata.mock.keyPoints
+            $0.coverImageUrl = URL(string: Metadata.mock.imageUrl)
+            let currentChapter = Metadata.mock.keyPoints.first!
+            $0.currentChapter = currentChapter
+            $0.playerState.currentTrack = URL(string: currentChapter.fileUrl)
+            $0.chaptersCount = "KEY POINT 1 OF 2"
+        }
+    }
+    
+    @MainActor
+    func testSelectingChapter() async {
+        let clock = TestClock()
+        let currentChapter = Metadata.mock.keyPoints.first!
+        let state = BookPlayMainReducer.State.init(metadataUrlString: "url",
+                                                   downloadMode: .downloaded,
+                                                   coverImageUrl: nil,
+                                                   keyPoints: Metadata.mock.keyPoints,
+                                                   isLyricsScreenMode: false,
+                                                   currentChapter: currentChapter,
+                                                   playerState: .init(currentTrack: URL(string: currentChapter.fileUrl)))
+        
+        let store = TestStore(initialState: state) {
+            BookPlayMainReducer()
+        } withDependencies: {
+            $0.continuousClock = clock
+        }
+        
+        await store.send(.playerAction(.nextTrack)) {
+            let currentChapter = Metadata.mock.keyPoints.last!
+            $0.currentChapter = currentChapter
+            $0.playerState.isLoadingTrackInfo = false
+            $0.playerState.currentTrack = URL(string: currentChapter.fileUrl)
+            $0.chaptersCount = "KEY POINT 2 OF 2"
+        }
+        
+        await store.receive(\.playerAction.loadTrackInfo) {
+            $0.playerState.isLoadingTrackInfo = true
+        }
+        
+        await store.receive(\.playerAction.totalTime) {
+            $0.playerState.isLoadingTrackInfo = false
+        }
+        
+        await clock.advance(by: .seconds(1))
+        await store.receive(\.playerAction.tick)
+        await store.receive(\.playerAction.nextTrack)
+        
+        await store.finish()
+    }
+    
+}
---
+++ BookPlayTCATests/BookPlayerComponentTests.swift
@@ -0,0 +1,46 @@
+//
+//  BookPlayerComponentTests.swift
+//  BookPlayTCATests
+//
+//  Created by Mykola Hrybeniuk on 05.08.2024.
+//
+
+import ComposableArchitecture
+import XCTest
+@testable import BookPlayTCA
+
+final class BookPlayerComponentTests: XCTestCase {
+
+    @MainActor
+    func testPlayer() async {
+        let clock = TestClock()
+        let state = BookPlayerComponentReducer.State.init(currentTrack: URL(string: "TrackUrl"))
+        
+        let store = TestStore(initialState: state) {
+            BookPlayerComponentReducer()
+        }  withDependencies: {
+            $0.continuousClock = clock
+        }
+        
+        await store.send(.viewOnAppear)
+        
+        await store.receive(\.playPauseTapped) {
+            $0.isPlaying = true
+        }
+        
+        await store.receive(\.loadTrackInfo) {
+            $0.isLoadingTrackInfo = true
+        }
+        
+        await store.receive(\.totalTime) {
+            $0.isLoadingTrackInfo = false
+        }
+        
+        await store.send(.pause) {
+            $0.isPlaying = false
+        }
+        
+        await store.finish()
+    }
+    
+}
